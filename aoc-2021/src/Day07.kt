7922110174a2d22167c3118a38ed293b1f21ed69
Callum Fairbairn <callumjoelfairbairn@gmail.com>
2023-06-14 17:01:14 +0100

    2021/07

---
+++ aoc-2021/src/Day07.kt
@@ -0,0 +1,42 @@
+import kotlin.math.abs
+
+private val testInput = readInput("Day07_test")
+private val realInput = readInput("Day07")
+
+private fun triangle(n: Int): Int {
+    val nPlusOne = n + 1
+    return n * nPlusOne / 2
+}
+
+fun main(args: Array<String>) {
+    fun part1(input: List<String>): Int {
+        val xValues = input[0].split(',').map { it.toInt() }.sorted()
+        val min = xValues.min()
+        val max = xValues.max()
+
+        return (min..max).fold(Int.MAX_VALUE) { minTotal, x ->
+            val totalDistance = xValues.fold(0) { acc, i ->
+                acc + abs(x - i)
+            }
+            if(totalDistance < minTotal) totalDistance else minTotal
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val xValues = input[0].split(',').map { it.toInt() }.sorted()
+        val min = xValues.min()
+        val max = xValues.max()
+
+        return (min..max).fold(Int.MAX_VALUE) { minTotal, x ->
+            val totalDistance = xValues.fold(0) { acc, i ->
+                acc + triangle(abs(x - i))
+            }
+            if(totalDistance < minTotal) totalDistance else minTotal
+        }
+    }
+
+    println(part1(testInput))
+    println(part1(realInput))
+    println(part2(testInput))
+    println(part2(realInput))
+}
