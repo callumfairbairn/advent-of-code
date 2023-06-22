e1e9286afb5c58ca52d5435e82a466aa6604b168
Callum Fairbairn <callumjoelfairbairn@gmail.com>
2023-06-22 17:58:19 +0100

    2021/Day12 part 2 logic

--- aoc-2021/src/Day12.kt
+++ aoc-2021/src/Day12.kt
@@ -35,8 +35,8 @@ class CaveSystem(input: List<String>) {
         }
         input.forEach { line ->
             val (cave1, cave2) = line.split("-").map { getCave(it) }
-            cave1.addConnection(cave2)
-            cave2.addConnection(cave1)
+            if(cave2.name != "start") { cave1.addConnection(cave2) }
+            if(cave1.name != "start") { cave2.addConnection(cave1) }
         }
     }
 
@@ -53,6 +53,36 @@ class CaveSystem(input: List<String>) {
         val newPaths = possibleNeighbors.map { getAllPaths(it, allPaths, (currentPath + it).toMutableList()) }
         return newPaths.flatten().toMutableSet()
     }
+
+    fun getAllPaths2(
+        start: Cave,
+        allPaths: MutableSet<MutableList<Cave>> = mutableSetOf(),
+        currentPath: MutableList<Cave> = mutableListOf(start),
+        beenToASmallCaveTwice: Boolean = false,
+    ): MutableSet<MutableList<Cave>> {
+        if (start == getCave("end")) {
+            allPaths.add(currentPath)
+            return allPaths
+        }
+        val hasBeenToASmallCaveTwice = beenToASmallCaveTwice ||
+                currentPath
+                    .filter { it != Cave("start") && !it.bigCave }
+                    .groupingBy { it }
+                    .eachCount()
+                    .values.any { it > 1 }
+        val possibleNeighbors = start.connections.filter {
+            it.bigCave || !hasBeenToASmallCaveTwice || !currentPath.contains(it)
+        }
+        val newPaths = possibleNeighbors.map {
+            getAllPaths2(
+                it,
+                allPaths,
+                (currentPath + it).toMutableList(),
+                hasBeenToASmallCaveTwice
+            )
+        }
+        return newPaths.flatten().toMutableSet()
+    }
 }
 
 fun main() {
@@ -64,11 +94,14 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val caveSystem = CaveSystem(input)
+        caveSystem.initiateCaveSystem(input)
+        val allPaths = caveSystem.getAllPaths2(caveSystem.getCave("start"))
+        return allPaths.size
     }
 
-    println(part1(testInput))
-    println(part1(realInput))
+//    println(part1(testInput))
+//    println(part1(realInput))
     println(part2(testInput))
     println(part2(realInput))
 }
