46be39033492a11e2cb2f52ae2ef5f96101d394f
Callum Fairbairn <callumjoelfairbairn@gmail.com>
2023-08-17 22:12:53 +0100

    2021/Day16

---
+++ aoc-2021/src/Day16.kt
@@ -0,0 +1,17 @@
+private val testInput = readInput("Day16_test")
+private val realInput = readInput("Day16")
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    println(part1(testInput))
+    println(part1(realInput))
+    println(part2(testInput))
+    println(part2(realInput))
+}
