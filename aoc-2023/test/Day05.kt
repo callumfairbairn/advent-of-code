4101b60ba4cf020e7ab8075838994c72840bd7b0
Callum Fairbairn <callumjoelfairbairn@gmail.com>
2024-01-07 10:40:01 +0000

    BigInteger.valueOf -> .toBigInteger

--- aoc-2023/test/Day05.kt
+++ aoc-2023/test/Day05.kt
@@ -1,4 +1,3 @@
-import java.math.BigInteger
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -7,37 +6,37 @@ internal class TestSourceToDestinationMap {
     fun testOneToOneMap() {
         val testInput = listOf("1 1 3")
         val sToDMap = SourceToDestinationMap(testInput)
-        assertEquals(BigInteger.valueOf(1), sToDMap.get(BigInteger.valueOf(1)))
-        assertEquals(BigInteger.valueOf(2), sToDMap.get(BigInteger.valueOf(2)))
-        assertEquals(BigInteger.valueOf(3), sToDMap.get(BigInteger.valueOf(3)))
-        assertEquals(BigInteger.valueOf(4), sToDMap.get(BigInteger.valueOf(4)))
+        assertEquals(1.toBigInteger(), sToDMap.get(1.toBigInteger()))
+        assertEquals(2.toBigInteger(), sToDMap.get(2.toBigInteger()))
+        assertEquals(3.toBigInteger(), sToDMap.get(3.toBigInteger()))
+        assertEquals(4.toBigInteger(), sToDMap.get(4.toBigInteger()))
     }
 
     @Test
     fun testUnequalSourceAndDestination() {
         val testInput = listOf("10 1 3")
         val sToDMap = SourceToDestinationMap(testInput)
-        assertEquals(BigInteger.valueOf(10), sToDMap.get(BigInteger.valueOf(1)))
-        assertEquals(BigInteger.valueOf(11), sToDMap.get(BigInteger.valueOf(2)))
-        assertEquals(BigInteger.valueOf(12), sToDMap.get(BigInteger.valueOf(3)))
-        assertEquals(BigInteger.valueOf(4), sToDMap.get(BigInteger.valueOf(4)))
+        assertEquals(10.toBigInteger(), sToDMap.get(1.toBigInteger()))
+        assertEquals(11.toBigInteger(), sToDMap.get(2.toBigInteger()))
+        assertEquals(12.toBigInteger(), sToDMap.get(3.toBigInteger()))
+        assertEquals(4.toBigInteger(), sToDMap.get(4.toBigInteger()))
     }
 
     @Test
     fun testFillsInTheGaps() {
         val testInput = listOf("10 3 3", "20 8 2")
         val sToDMap = SourceToDestinationMap(testInput)
-        assertEquals(BigInteger.valueOf(0), sToDMap.get(BigInteger.valueOf(0)))
-        assertEquals(BigInteger.valueOf(1), sToDMap.get(BigInteger.valueOf(1)))
-        assertEquals(BigInteger.valueOf(2), sToDMap.get(BigInteger.valueOf(2)))
-        assertEquals(BigInteger.valueOf(10), sToDMap.get(BigInteger.valueOf(3)))
-        assertEquals(BigInteger.valueOf(11), sToDMap.get(BigInteger.valueOf(4)))
-        assertEquals(BigInteger.valueOf(12), sToDMap.get(BigInteger.valueOf(5)))
-        assertEquals(BigInteger.valueOf(6), sToDMap.get(BigInteger.valueOf(6)))
-        assertEquals(BigInteger.valueOf(7), sToDMap.get(BigInteger.valueOf(7)))
-        assertEquals(BigInteger.valueOf(20), sToDMap.get(BigInteger.valueOf(8)))
-        assertEquals(BigInteger.valueOf(21), sToDMap.get(BigInteger.valueOf(9)))
-        assertEquals(BigInteger.valueOf(10), sToDMap.get(BigInteger.valueOf(10)))
+        assertEquals(0.toBigInteger(), sToDMap.get(0.toBigInteger()))
+        assertEquals(1.toBigInteger(), sToDMap.get(1.toBigInteger()))
+        assertEquals(2.toBigInteger(), sToDMap.get(2.toBigInteger()))
+        assertEquals(10.toBigInteger(), sToDMap.get(3.toBigInteger()))
+        assertEquals(11.toBigInteger(), sToDMap.get(4.toBigInteger()))
+        assertEquals(12.toBigInteger(), sToDMap.get(5.toBigInteger()))
+        assertEquals(6.toBigInteger(), sToDMap.get(6.toBigInteger()))
+        assertEquals(7.toBigInteger(), sToDMap.get(7.toBigInteger()))
+        assertEquals(20.toBigInteger(), sToDMap.get(8.toBigInteger()))
+        assertEquals(21.toBigInteger(), sToDMap.get(9.toBigInteger()))
+        assertEquals(10.toBigInteger(), sToDMap.get(10.toBigInteger()))
     }
 
 }
