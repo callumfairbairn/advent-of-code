d152e73394e54b9fe96b908110d1fec22f9b584a
Callum Fairbairn <callumjoelfairbairn@gmail.com>
2024-12-11 09:47:26 +0000

    Delete Main and add Day04

---
+++ aoc-2024/src/main/java/com/Day04.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.util.List;
+
+public class Day04 {
+  public Day04(List<String> input) {
+    parseInput(input);
+  }
+
+  void parseInput(List<String> input) {
+
+  }
+
+  public int part1() {
+    return 0;
+  }
+
+  public int part2() {
+    return 0;
+  }
+}
