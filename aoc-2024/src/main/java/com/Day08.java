2507d16ab58f1fabaaa3d8e8f6b1466eb4202b92
callumfairbairn <callum_fairbairn@hotmail.com>
2025-03-05 19:17:25 +0000

    2024 Day08

--- aoc-2024/src/main/java/com/Day08.java
+++ aoc-2024/src/main/java/com/Day08.java
@@ -1,13 +1,13 @@
 package com;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Day08 {
   List<List<String>> grid = new ArrayList<>();
-  List<List<String>> antinodes = new ArrayList<>();
+  HashSet<Coord> antinodes = new HashSet<>();
   HashMap<String, List<Coord>> reverseGrid = new HashMap<>();
+
+  boolean getAllAntinodesOn;
 
   public Day08(List<String> input) {
     parseInput(input);
@@ -17,30 +17,85 @@ public class Day08 {
     for (var i = 0; i < input.size(); i++) {
       var split = List.of(input.get(i).split(""));
       grid.add(split);
-      antinodes.add(List.of(new String[input.get(i).length()]));
 
       for (var j = 0; j < split.size(); j++) {
         var freq = split.get(j);
-        reverseGrid.putIfAbsent(freq, new ArrayList<>());
-        reverseGrid.get(freq).add(new Coord(i, j));
-      }
-    }
-  }
-
-  public int part1() {
-    for (var i = 0; i < grid.size(); i++) {
-      for (var j = 0; j < grid.get(i).size(); j++) {
-        var freq = grid.get(i).get(j);
         if (!freq.equals(".")) {
-
+          reverseGrid.putIfAbsent(freq, new ArrayList<>());
+          reverseGrid.get(freq).add(new Coord(i, j));
         }
       }
     }
+  }
 
-    return 0;
+  boolean nodeInGrid(Coord node) {
+    return node.x() >= 0 && node.x() < grid.size() && node.y() >= 0 && node.y() < grid.getFirst().size();
+  }
+
+   public static List<Coord> getAntinodes(Coord a, Coord b) {
+    var dX = a.x() - b.x();
+    var dY = a.y() - b.y();
+
+    var node1 = new Coord(a.x() + dX, a.y() + dY);
+    var node2 = new Coord(b.x() - dX, b.y() - dY);
+    return Arrays.asList(node1, node2);
+  }
+
+  public List<Coord> getAllAntinodes(Coord a, Coord b) {
+    var dX = a.x() - b.x();
+    var dY = a.y() - b.y();
+
+    var antinodes = new ArrayList<Coord>();
+
+    var currentPositiveDir = new Coord(b.x() + dX, b.y() + dY);
+    while (nodeInGrid(currentPositiveDir)) {
+      antinodes.add(currentPositiveDir);
+      currentPositiveDir = new Coord(currentPositiveDir.x() + dX, currentPositiveDir.y() + dY);
+    }
+
+    var currentNegativeDir = new Coord(a.x() - dX, a.y() - dY);
+    while (nodeInGrid(currentNegativeDir)) {
+      antinodes.add(currentNegativeDir);
+      currentNegativeDir = new Coord(currentNegativeDir.x() - dX, currentNegativeDir.y() - dY);
+    }
+
+    return antinodes;
+  }
+
+  void addAntinodes(Coord a, Coord b) {
+    var antinodesToAdd = getAllAntinodesOn ? getAllAntinodes(a, b).stream() : getAntinodes(a, b).stream().filter(this::nodeInGrid);
+    antinodesToAdd.forEach(antinodes::add);
+  }
+
+  void addAntinodesForEachCoordCombo(List<Coord> coords) {
+    for (var i = 0; i < coords.size(); i++) {
+      for (var j = 1; j < coords.size(); j++) {
+        if (j > i) {
+          addAntinodes(coords.get(i), coords.get(j));
+        }
+      }
+    }
+  }
+
+  Integer countNumberOfAntinodes() {
+    for (Map.Entry<String, List<Coord>> entry : reverseGrid.entrySet()) {
+      var coords = entry.getValue();
+
+      if (coords.size() > 1) {
+        addAntinodesForEachCoordCombo(coords);
+      }
+    }
+
+    return antinodes.size();
+  }
+
+  public int part1() {
+    getAllAntinodesOn = false;
+    return countNumberOfAntinodes();
   }
 
   public int part2() {
-    return 0;
+    getAllAntinodesOn = true;
+    return countNumberOfAntinodes();
   }
 }
--- aoc-2024/src/test/java/Day08Test.java
+++ aoc-2024/src/test/java/Day08Test.java
@@ -1,3 +1,4 @@
+import com.Coord;
 import com.Day08;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -5,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
 
+import static com.Day08.getAntinodes;
 import static com.Utils.readInput;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,20 +32,50 @@ public class Day08Test {
   void part1RealInput() {
     var day = new Day08(realInput);
     var result = day.part1();
-    assertEquals(0, result);
+    assertEquals(329, result);
   }
 
   @Test
   void part2TestInput() {
     var day = new Day08(testInput);
     var result = day.part2();
-    assertEquals(0, result);
+    assertEquals(34, result);
   }
 
   @Test
   void part2RealInput() {
     var day = new Day08(realInput);
     var result = day.part2();
-    assertEquals(0, result);
+    assertEquals(1190, result);
+  }
+
+  @Test
+  void testGetAntinodes1() {
+    var freqALocation = new Coord(4, 3);
+    var freqBLocation = new Coord(5, 5);
+
+    var antiondes = getAntinodes(freqALocation, freqBLocation);
+    assertEquals(new Coord(3, 1), antiondes.get(0));
+    assertEquals(new Coord(6, 7), antiondes.get(1));
+  }
+
+  @Test
+  void testGetAntinodes2() {
+    var freqALocation = new Coord(4, 3);
+    var freqBLocation = new Coord(8, 4);
+
+    var antiondes = getAntinodes(freqALocation, freqBLocation);
+    assertEquals(new Coord(0, 2), antiondes.get(0));
+    assertEquals(new Coord(12, 5), antiondes.get(1));
+  }
+
+  @Test
+  void testGetAntinodes3() {
+    var freqALocation = new Coord(5, 5);
+    var freqBLocation = new Coord(8, 4);
+
+    var antiondes = getAntinodes(freqALocation, freqBLocation);
+    assertEquals(new Coord(2, 6), antiondes.get(0));
+    assertEquals(new Coord(11, 3), antiondes.get(1));
   }
 }
